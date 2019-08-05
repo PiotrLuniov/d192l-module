7991da480fe42623f4c8d63550cb5daa6e928b23
Kirill Shevchenko <kirill_shevchenko1@epam.com>
2019-08-05 20:40:53 +0300

    initial

--- jobs.groovy
+++ jobs.groovy
@@ -1,98 +1,81 @@
-def student = "kshevchenko"
-def mainjob = "MNTLAB-" + student + "-main-build-job"
 
-job(mainjob) {
-    description()
-    keepDependencies(false)
+job('MNTLAB-kshevchenko-main-build-job') {
     parameters {
-        //jobs execute
-        activeChoiceParam('JOBS') {
-            description('Choose execute jobs name')
+        activeChoiceParam('BRANCH_NAME') {
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('return ["kshevchenko", "master"]')
+                fallbackScript('return ["error"]')
+            }
+        }
+        activeChoiceReactiveParam('BUILDS_TRIGGER') {
             choiceType('CHECKBOX')
             groovyScript {
                 script('''
-def jobs = []
-(1..4).each {
-  jobs.add("MNTLAB-ashamchonak-child${it}-build-job")
-}
-return jobs
-                			'''
-                )
-                fallbackScript('"fallback choice"')
+                def list=[];
+                for (i in 1..4){
+                list.add("MNTLAB-kshevchenko-child"+i+"-build-job");
+                }
+                return list;
+                ''')
+                fallbackScript('"error"')
+                referencedParameter('BRANCH_NAME')
             }
         }
-        //branch name
-        choiceParam("BRANCH_NAME", [student, "master"])
     }
-
-    blockOnDownstreamProjects()
-
-    steps {
+    publishers {
         downstreamParameterized {
-            trigger("\$JOBS") {
-                block {
-                    buildStepFailure('FAILURE')
-                    failure('FAILURE')
-                    unstable('UNSTABLE')
-                }
+            trigger('$BUILDS_TRIGGER') {
+                condition('UNSTABLE_OR_BETTER')
                 parameters {
-                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+                    predefinedBuildParameters {
+                        properties('BRANCH_NAME=$BRANCH_NAME')
+                        textParamValueOnNewLine(true)
+
+                    }
                 }
             }
-
         }
     }
-}
 
-for (i in (1..4)) {
-    job("MNTLAB-" + student + "-child${i}-build-job") {
-        description()
-        keepDependencies(false)
-        disabled(false)
-        concurrentBuild(false)
+}
+for(i in 1..4){
+    job('MNTLAB-kshevchenko-child'+i+'-build-job'){
         parameters {
             activeChoiceParam('BRANCH_NAME') {
                 choiceType('SINGLE_SELECT')
                 groovyScript {
                     script('''
-def gitURL = "https://github.com/MNT-Lab/d192l-module.git"
-def command = "git ls-remote --heads $gitURL"
-def proc = command.execute()
-proc.waitFor()
-def branches = proc.in.text.readLines().collect{
-it.split('/')[-1]
-}
-return branches
-					''')
-                    fallbackScript('"No branches"')
+                def gitURL = "https://github.com/MNT-Lab/d192l-module.git"
+                def command = "git ls-remote -h $gitURL"
+                def proc = command.execute()
+                proc.waitFor()
+                if ( proc.exitValue() != 0 ) {
+                println "Error, ${proc.err.text}"
+                System.exit(-1)
+                }
+                def branches = proc.in.text.readLines().collect {
+                it.replaceAll(/[a-z0-9]*\\trefs\\/heads\\//, '')
+                }
+                return branches;
+                ''')
+                    fallbackScript('"error"')
                 }
             }
         }
 
         scm {
-            git {
-                remote {
-                    github("MNT-Lab/d192l-module", "https")
-                }
-                branch("*/" + student)
+            git('https://github.com/MNT-Lab/d192l-module.git', '$BRANCH_NAME')
+        }
+        steps {
+            shell {
+                command('chmod +x script.sh  &&  ./script.sh > output.txt && tar czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy')
             }
         }
-        disabled(false)
-        concurrentBuild(false)
-        steps {
-            shell("chmod +x script.sh")
-            shell("echo \$(./script.sh) > output.txt")
-            shell("tar -czvf \${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy")
-        }
-
         publishers {
             archiveArtifacts {
-                pattern("output.txt")
-                pattern("\${BRANCH_NAME}_dsl_script.tar.gz")
-                allowEmpty(false)
-                onlyIfSuccessful(false)
-                fingerprint(false)
-                defaultExcludes(true)
+                pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+                onlyIfSuccessful()
             }
         }
     }
