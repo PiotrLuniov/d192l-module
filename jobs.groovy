f8e423e98085147d1309bf26a85674412ecad707
Kiryl Kaminski <kaminsky-kirill@tut.by>
2019-08-05 12:57:58 +0300

    active choice

--- jobs.groovy
+++ jobs.groovy
@@ -1,7 +1,14 @@
 job('MNTLAB-kkaminski-main-build-job') {
     
   parameters {
-        
-        choiceParam('BRANCH_NAME', ['kkaminski (default)', 'master'])
+           
+      choiceParam('BRANCH_NAME', ['kkaminski (default)', 'master'])
+       
+      activeChoiceParam('BUILDS_TRIGGER') {
+            description('Available options')
+            choiceType('CHECKBOX')
+            
+        }
+    
     }
 }
