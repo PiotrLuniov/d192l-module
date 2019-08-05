d33834c9b590c720dd390d74c6d0b720def191bd
pavel.romanowski <33510707+subzik@users.noreply.github.com>
2019-08-06 01:46:44 +0300

    steps

--- jobs.groovy
+++ jobs.groovy
@@ -1,30 +1,24 @@
 job('MNTLAB-pramanouski-main-build-job') {
+  blockOnDownstreamProjects()
 
   parameters {
-
       choiceParam('BRANCH_NAME', ['pramanouski (default)', 'master'])
 
       activeChoiceParam('BUILDS_TRIGGER') {
             description('Available options')
             choiceType('CHECKBOX')
-
             groovyScript {
                 script('''
-
              def list=[]
 	         (1..4).each {
 	         list.add("MNTLAB-pramanouski-child${it}-build-job")
        }
 	      return list
-
-            ''')
-
+          ''')
         }
 
     }
   }
-}
-
 
   steps {
         downstreamParameterized {
@@ -41,4 +35,48 @@ job('MNTLAB-pramanouski-main-build-job') {
             }
         }
     }
+}
+
+  or(i in 1..4) {
+    job("MNTLAB-pramanouski-child${i}-build-job") {
+      parameters {
+        activeChoiceParam('BRANCH_NAME') {
+              description('List of branches')
+              choiceType('SINGLE_SELECT')
+
+              groovyScript {
+                  script('''
+               def gitURL = "https://github.com/MNT-Lab/d192l-module.git"
+               def command = "git ls-remote -h $gitURL"
+               def proc = command.execute()
+               proc.waitFor()
+               def branches = proc.in.text.readLines().collect {
+                   it.replaceAll(/[a-z0-9]*\trefs\\/heads\\//, '')
+               }
+               return branches
+              ''')
+              }
+         }
+      }
+
+  scm {
+    git {
+      remote {
+        url('https://github.com/MNT-Lab/d192l-module.git')
+      }
+      branch("\${BRANCH_NAME}")
+    }
+
+    }
+    steps {
+    shell ('chmod +x script.sh  &&  ./script.sh > output.txt && tar czvf ${BRANCH_NAME}_dsl_script.tar.gz output.txt jobs.groovy')
+    }
+
+    publishers {
+      archiveArtifacts {
+      pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+      onlyIfSuccessful()
+      }
+    }
   }
+}
