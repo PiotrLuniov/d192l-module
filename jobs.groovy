86062f9c3f73d9849887507b8c279fa29d004d66
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-08-05 15:39:44 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,12 +1,6 @@
-String student = "ashamchonak"
-String mainjob = "MNTLAB-" + student + "-main-build-job"
-String childjob = "MNTLAB-" + student + "-child1-build-job"
-
-job(mainjob) {
-	description()
-	keepDependencies(false)String student = "ashamchonak"
-String mainjob = "MNTLAB-" + student + "-main-build-job"
-String childjob = "MNTLAB-" + student + "-child1-build-job"
+def student = "ashamchonak"
+def mainjob = "MNTLAB-" + student + "-main-build-job"
+def childjob = "MNTLAB-" + student + "-child1-build-job"
 
 job(mainjob) {
 	description()
@@ -27,47 +21,7 @@ job(mainjob) {
 	steps {
 		shell("git ls-remote --heads  https://github.com/MNT-Lab/d192l-module.git")
         shell("chmod +x script.sh")
-        shell("./script.sh")
-	}
-}
-
-job(childjob) {
-	description()
-	keepDependencies(false)
-	disabled(false)
-	concurrentBuild(false)
-	steps {
-		shell("""git ls-remote --heads  https://github.com/MNT-Lab/d192l-module.git
-
-          ./script.sh > output.txt""")
-	}
-	publishers {
-		archiveArtifacts {
-			pattern("output.txt,")
-			allowEmpty(false)
-			onlyIfSuccessful(false)
-			fingerprint(false)
-			defaultExcludes(true)
-		}
-	}
-}
-	parameters {
-		stringParam("BRANCH_NAME", student, "")
-	}
-	scm {
-		git {
-			remote {
-				github("MNT-Lab/d192l-module", "https")
-			}
-			branch("*/student")
-		}
-	}
-	disabled(false)
-	concurrentBuild(false)
-	steps {
-		shell("git ls-remote --heads  https://github.com/MNT-Lab/d192l-module.git")
-        shell("chmod +x script.sh")
-        shell("./script.sh")
+        shell("echo $(./script.sh)")
 	}
 }
 
