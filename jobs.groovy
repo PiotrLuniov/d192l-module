212dab26d562570342e482147bf545517ddf14bd
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-08-05 15:52:39 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -6,7 +6,17 @@ job(mainjob) {
 	description()
 	keepDependencies(false)
 	parameters {
+        activeChoiceParam('CHOICE-1') {
+        description('Allows user choose from multiple choices')
+        filterable()
+        choiceType('SINGLE_SELECT')
+        groovyScript {
+          	script('["choice1", "choice2"]')
+            fallbackScript('"fallback choice"')
+            }
+		}
 		stringParam("BRANCH_NAME", student, "")
+		
 	}
 	scm {
 		git {
