f7dfc572ac7201d08c56c50ac98080d7a488af56
Ilya <buagir1990@gmail.com>
2019-08-05 17:34:41 +0300

    6th attempt

--- jobs.groovy
+++ jobs.groovy
@@ -1,37 +1,87 @@
 job('MNTLAB-iyaruk-main-build-job') {
+    description('Main job')
 
-  parameters {
-      choiceParam('BRANCH_NAME', ['iyaruk', 'master'])
-
-      activeChoiceParam('BUILDS_JOBS') {
-            description('Job for works')
+    parameters {
+        choiceParam('BRANCH_NAME', ['iyaruk', 'master'])
+        activeChoiceParam('EXECUTED_JOBS') {
+            description('Main job for working with childs jobs')
             choiceType('CHECKBOX')
-            
             groovyScript {
                 script('''
-                 def list=[]
-	         (1..4).each {
-	         list.add("MNTLAB-iyaruk-child${it}-build-job")
-       }
-	      return list
-            ''')
-
+                    def list = []
+                    (1..4).each {
+                        list.add("MNTLAB-iyaruk-child${it}-build-job")
+                    }
+                    return list
+                ''')
+            }
         }
     }
-	scm {
+
+    blockOnDownstreamProjects()
+
+    steps {
+        downstreamParameterized {
+            trigger('$EXECUTED_JOBS') {
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+                parameters {
+                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+                }
+            }
+        }
+    }
+}
+
+for ( i in (1..4) ) {
+    job("MNTLAB-iyaruk-child${i}-build-job") {
+        description('Child job')
+
+        parameters {
+            activeChoiceParam('BRANCH_NAME') {
+                description('Click for choiceing job')
+                choiceType('SINGLE_SELECT')
+
+                groovyScript {
+                    script('''
+                        def Git = "https://github.com/MNT-Lab/d192l-module.git"
+                        def task = "git ls-remote -h $Git"
+                        def brot = task.execute()
+                        brot.waitFor()
+                        def trees = brot.in.text.readLines().collect{
+                            it.split('/')[-1]
+                        }
+                        return trees
+                    ''')
+                }
+            }
+        }
+
+        scm {
             git {
                 remote {
                     name('branch')
                     url('https://github.com/MNT-Lab/d192l-module.git')
                 }
-                branch("\$BRANCH_NAME")
+                branch('$BRANCH_NAME')
             }
         }
-      triggers {
-          scm('H/15 * * * *')
-      }
-      steps {
-      	  shell('bash ./script.sh')
-      }
-  }
-}
+
+        steps {
+            shell('''
+            bash script.sh > output.txt
+            tar czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy
+            ''')
+        }
+
+        publishers {
+            archiveArtifacts {
+                pattern('output.txt')
+                pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+                onlyIfSuccessful()
+            }
+        }
+    }
