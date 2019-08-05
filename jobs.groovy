d9cf6de795abf3f3b6aa010d8d67250cc7436a67
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-08-05 15:37:07 +0300

    Update jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -4,7 +4,53 @@ String childjob = "MNTLAB-" + student + "-child1-build-job"
 
 job(mainjob) {
 	description()
+	keepDependencies(false)String student = "ashamchonak"
+String mainjob = "MNTLAB-" + student + "-main-build-job"
+String childjob = "MNTLAB-" + student + "-child1-build-job"
+
+job(mainjob) {
+	description()
 	keepDependencies(false)
+	parameters {
+		stringParam("BRANCH_NAME", student, "")
+	}
+	scm {
+		git {
+			remote {
+				github("MNT-Lab/d192l-module", "https")
+			}
+			branch("*/" + student)
+		}
+	}
+	disabled(false)
+	concurrentBuild(false)
+	steps {
+		shell("git ls-remote --heads  https://github.com/MNT-Lab/d192l-module.git")
+        shell("chmod +x script.sh")
+        shell("./script.sh")
+	}
+}
+
+job(childjob) {
+	description()
+	keepDependencies(false)
+	disabled(false)
+	concurrentBuild(false)
+	steps {
+		shell("""git ls-remote --heads  https://github.com/MNT-Lab/d192l-module.git
+
+          ./script.sh > output.txt""")
+	}
+	publishers {
+		archiveArtifacts {
+			pattern("output.txt,")
+			allowEmpty(false)
+			onlyIfSuccessful(false)
+			fingerprint(false)
+			defaultExcludes(true)
+		}
+	}
+}
 	parameters {
 		stringParam("BRANCH_NAME", student, "")
 	}
