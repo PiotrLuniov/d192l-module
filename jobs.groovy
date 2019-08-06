14a93bfd4adb3008435a254d2f0836987e6b18bb
Siarhei_Kuzmich <siarhei_kuzmich@epam.com>
2019-08-06 15:15:49 +0300

    groovy

---
+++ jobs.groovy
@@ -0,0 +1,82 @@
+job('MNTLAB-skuzmich-main-build-job') {
+  blockOnDownstreamProjects()
+
+  parameters {
+      choiceParam('BRANCH_NAME', ['skuzmich (default)', 'master'])
+
+      activeChoiceParam('BUILDS_TRIGGER') {
+            description('Available options')
+            choiceType('CHECKBOX')
+            groovyScript {
+                script('''
+             def list=[]
+	         (1..4).each {
+	         list.add("MNTLAB-skuzmich-child${it}-build-job")
+       }
+	      return list
+          ''')
+        }
+
+    }
+  }
+
+  steps {
+        downstreamParameterized {
+            trigger('$BUILDS_TRIGGER') {
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+                parameters {
+                  predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+
+               }
+            }
+        }
+    }
+}
+
+for(i in 1..4) {
+  job("MNTLAB-skuzmich-child${i}-build-job") {
+    parameters {
+      activeChoiceParam('BRANCH_NAME') {
+            description('List of branches')
+            choiceType('SINGLE_SELECT')
+
+            groovyScript {
+                script('''
+             def gitURL = "https://github.com/MNT-Lab/d192l-module.git"
+             def command = "git ls-remote -h $gitURL"
+             def proc = command.execute()
+             proc.waitFor()
+             def branches = proc.in.text.readLines().collect {
+                 it.replaceAll(/[a-z0-9]*\trefs\\/heads\\//, '')
+             }
+             return branches
+            ''')
+            }
+       }
+    }
+
+  scm {
+    git {
+      remote {
+        url('https://github.com/MNT-Lab/d192l-module.git')
+      }
+      branch("\${BRANCH_NAME}")
+    }
+
+    }
+    steps {
+    shell ('chmod +x script.sh  &&  ./script.sh > output.txt && tar czvf ${BRANCH_NAME}_dsl_script.tar.gz output.txt jobs.groovy')
+    }
+
+    publishers {
+      archiveArtifacts {
+      pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+      onlyIfSuccessful()
+      }
+    }
+  }
+}
